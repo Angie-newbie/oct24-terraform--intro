0dbc3eabb13e1b6bdaff2af4f3bc41227674ab43
Angie <angel890817@hotmail.com>
2025-07-24 15:42:06 +1000

    feat: terraform output and random bucket name

--- main.tf
+++ main.tf
@@ -3,5 +3,9 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "demo_bucket_angie-was-here" {
-    bucket = "some-cool-unique-bucket-name-here"
+    bucket = "some-cool-unique-bucket-name-here-${random_id.bucket_id_random.hex}"
+}
+
+resource "random_id" "bucket_id_random" {
+  byte_length = 4
 }
---
+++ output.tf
@@ -0,0 +1,5 @@
+output "bucket_name" {
+    description = "The name of the bucket"
+    value = aws_s3_bucket.demo_bucket_angie-was-here.bucket
+  
+}
